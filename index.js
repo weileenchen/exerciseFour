c262fb60abb3ec7383520f186027aac0e9b63b7d
weileenchen <wc1627@nyu.edu>
2021-10-27 18:46:48 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+const indexRoute = require("./routes/index");
+
+app.use("/", indexRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
+});
