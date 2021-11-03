a9bb65a32228c04a9d04e0cbff0b0b8c69ea0c33
weileenchen <wc1627@nyu.edu>
2021-11-03 18:53:59 -0400

    routes

--- routes/createArticle.js
+++ routes/createArticle.js
@@ -3,7 +3,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World in Route!");
+  res.send(`
+  <h1>Create Article</h1>
+  <form>
+    <label for="articleTitle"> Article Title </label>
+    <input type="text" name="articleTitle" placeholder="type article title"/>
+    <button type="submit">Submit Article </button>
+  </form>
+  `);
 });
 
 module.exports = router;
